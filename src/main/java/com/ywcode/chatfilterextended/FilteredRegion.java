8e2d78d1413e6fc62ee8a61dc3b1de94fa85bbf7
YvesW <7929021+YvesW@users.noreply.github.com>
2024-09-01 18:54:28 +0200

    FilteredRegion: use EnumSet

--- src/main/java/com/ywcode/chatfilterextended/FilteredRegion.java
+++ src/main/java/com/ywcode/chatfilterextended/FilteredRegion.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashSet;
+import java.util.EnumSet;
 import java.util.Set;
 
 @Getter(AccessLevel.PACKAGE)
@@ -13,16 +13,16 @@ public class FilteredRegion {
 
     private final int regionID;
     private boolean publicChatCustomOnly; //Only activate custom, don't check sets
-    private Set<ChatTabFilterOptionsOH> publicChatSetOH = new HashSet<>(); //Set is always initialized, but empty when it should not be used
-    private Set<ChatTabFilterOptions> publicChatSet = new HashSet<>(); //Set is always initialized, but empty when it should not be used
+    private Set<ChatTabFilterOptionsOH> publicChatSetOH = EnumSet.noneOf(ChatTabFilterOptionsOH.class); //Set is always initialized, but empty when it should not be used
+    private Set<ChatTabFilterOptions> publicChatSet = EnumSet.noneOf(ChatTabFilterOptions.class); //Set is always initialized, but empty when it should not be used
     private boolean privateChatCustomOnly; //Only activate custom, don't check sets
-    private Set<ChatTabFilterOptions> privateChatSet = new HashSet<>(); //Set is always initialized, but empty when it should not be used
+    private Set<ChatTabFilterOptions> privateChatSet = EnumSet.noneOf(ChatTabFilterOptions.class); //Set is always initialized, but empty when it should not be used
     private boolean channelChatCustomOnly; //Only activate custom, don't check sets
-    private Set<ChatTabFilterOptions> channelChatSet = new HashSet<>(); //Set is always initialized, but empty when it should not be used
+    private Set<ChatTabFilterOptions> channelChatSet = EnumSet.noneOf(ChatTabFilterOptions.class); //Set is always initialized, but empty when it should not be used
     private boolean clanChatCustomOnly; //Only activate custom, don't check sets
-    private Set<ChatTabFilterOptions> clanChatSet = new HashSet<>(); //Set is always initialized, but empty when it should not be used
+    private Set<ChatTabFilterOptions> clanChatSet = EnumSet.noneOf(ChatTabFilterOptions.class); //Set is always initialized, but empty when it should not be used
     private boolean tradeChatCustomOnly; //Only activate custom, don't check sets
-    private Set<ChatTabFilterOptions> tradeChatSet = new HashSet<>(); //Set is always initialized, but empty when it should not be used
+    private Set<ChatTabFilterOptions> tradeChatSet = EnumSet.noneOf(ChatTabFilterOptions.class); //Set is always initialized, but empty when it should not be used
 
     public FilteredRegion(int regionID) {
         this.regionID = regionID;
